36318ff3457731321afaf096f9ff8e7707a91ef6
davidlemure <davidl3mos@gmail.com>
2023-08-11 22:16:35 -0300

    Feat : Adicionou pasta notepage

--- NotePage.xaml.cs
+++ NotePage.xaml.cs
@@ -2,8 +2,34 @@ namespace notes;
 
 public partial class NotePage : ContentPage
 {
-	public NotePage()
-	{
-		InitializeComponent();
-	}
+
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+    {
+        InitializeComponent();
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+
+    }
+    public void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Salva o arquivo
+        File.WriteAllText (_fileName, TextEditor.Text);
+    }
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        //Deleta o arquivo
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+        TextEditor.Text = string.Empty;
+
+    }
+    
+    
+
+
+    
 }
+
+
